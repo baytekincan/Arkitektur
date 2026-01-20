7b13687a7be237b336aab60bbdc2febb1ebbb1ec
baytekincan <baytekincan20@gmail.com>
2026-01-20 17:08:49 +0300

    add FileService for S3 operations

---
+++ Arkitektur.Business/Services/FileServices/FileService.cs
@@ -0,0 +1,38 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using Arkitektur.Business.Base;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace Arkitektur.Business.Services.FileServices;
+
+public class FileService(IAmazonS3 _s3Client, IConfiguration _configuration) : IFileService
+{
+    private readonly string _bucketName = _configuration["AWS:BucketName"];
+    public async Task<BaseResult<object>> UploadImageToS3Async(IFormFile? file)
+    {
+        if (file is null || file.Length == 0)
+        {
+            return BaseResult<object>.Fail("No file provided or file is empty.");
+
+        }
+        await _s3Client.EnsureBucketExistsAsync(_bucketName);
+        var key = $"{Guid.NewGuid()}_{file.FileName}";
+
+        var request = new PutObjectRequest
+        {
+            BucketName = _bucketName,
+            Key = key,
+            InputStream = file.OpenReadStream(),
+            CannedACL = S3CannedACL.PublicRead
+        };
+
+        request.Metadata.Add("Content-Type", file.ContentType);
+        var response = await _s3Client.PutObjectAsync(request);
+
+        string fileUrl = $"https://{_bucketName}.s3.{_s3Client.Config.RegionEndpoint.SystemName}.amazonaws.com/{key}";
+
+        return BaseResult<object>.Success(new { imageUrl = fileUrl });
+
+    }
+}
---
+++ Arkitektur.Business/Services/FileServices/IFileService.cs
@@ -0,0 +1,8 @@
+﻿using Arkitektur.Business.Base;
+using Microsoft.AspNetCore.Http;
+namespace Arkitektur.Business.Services.FileServices;
+
+public interface IFileService
+{
+    Task<BaseResult<object>> UploadImageToS3Async(IFormFile? file);
+}
